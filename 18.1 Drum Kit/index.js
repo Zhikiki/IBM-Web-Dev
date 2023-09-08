8e8c2f0d59a70411033eb4c6a19f339b953686ac
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-09-08 10:33:02 +0200

    Added corresponding sound to the buttons

--- 18.1 Drum Kit/index.js
+++ 18.1 Drum Kit/index.js
@@ -1,5 +1,41 @@
 function handleClick() {
-  alert('I got click!');
+  var buttonInnerHtml = this.innerHTML;
+  switch (buttonInnerHtml) {
+    case 'w':
+      var crash = new Audio('./sounds/crash.mp3');
+      crash.play();
+      break;
+    case 'a':
+      var kick = new Audio('./sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    case 's':
+      var snare = new Audio('./sounds/snare.mp3');
+      snare.play();
+      break;
+    case 'd':
+      var tom1 = new Audio('./sounds/tom-1.mp3');
+      tom1.play();
+      break;
+
+    case 'j':
+      var tom2 = new Audio('./sounds/tom-2.mp3');
+      tom2.play();
+      break;
+
+    case 'k':
+      var tom3 = new Audio('./sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case 'l':
+      var tom4 = new Audio('./sounds/tom-4.mp3');
+      tom4.play();
+      break;
+      
+    default: console.log(buttonInnerHtml);
+  }
+  // this.style.color = 'white';
 }
 // Setting event listener OPTION 1
 // var set = document.querySelectorAll('.set button');
@@ -10,7 +46,8 @@ function handleClick() {
 // Setting event listener OPTION 2
 var set = document.querySelectorAll('.set button');
 for (let i = 0; i < set.length; i++) {
-    const button = set[i];
-    button.addEventListener('click', handleClick);
+  const button = set[i];
+  button.addEventListener('click', handleClick);
 }
-
+// var audio = new Audio('./sounds/tom-1.mp3');
+//   audio.play();
